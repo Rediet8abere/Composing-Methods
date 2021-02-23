05a08196735e2f90cd5b664816cee13471390f51
Rediet8abere <redietaberegesesse@gmail.com>
2021-02-23 13:07:02 -0800

    first commit

---
+++ rename_method.py
@@ -0,0 +1,28 @@
+# by Kami Bigdely
+# Rename Method
+# Reference: https://parade.com/1039985/marynliles/pick-up-lines/
+
+def cal_area_for_graph(graph):  
+    """Calculate the area under the input graph."""
+    # bla bla bla.
+    pass
+
+#####################
+
+def get_min_value(li):
+    m = li[0]
+    for value in li:
+        if value > m:
+            m = value
+    return m
+
+
+li = [5, -1, 43, 32, 87, -100]
+print(get_min_value(li))
+
+############################
+def split_sentence(sentence):
+    words = sentence[0:].split(' ')
+    return words
+
+print(split_sentence('If you were a vegetable, you’d be a ‘cute-cumber.'))
